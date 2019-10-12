69cb7d075951f1d2f2a4536684bbdb4cf5275960
Magnus Linell <magnus.linell@gmail.com>
2019-10-12 14:19:32 +0200

    Add logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import classNames from 'classnames';
 import styles from './Header.less';
 
 export default () => {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
+                <i className={classNames('fas', 'fa-skiing-nordic', styles.icon)}></i>
                 <Link href="/"><a className={styles.link}>Vallamannen</a></Link>
                 <Link href="/vasaloppet"><a className={styles.link}>Vasaloppet</a></Link>
                 <Link href="/vallaservice"><a className={styles.link}>Vallaservice</a></Link>
