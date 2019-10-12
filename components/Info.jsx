2c21e891e72bc3898aaf3bcebc2a07ce7b1c2276
Magnus Linell <magnus.linell@gmail.com>
2019-10-12 14:14:27 +0200

    fixed text

--- components/Info.jsx
+++ components/Info.jsx
@@ -8,7 +8,7 @@ export default ({ className }) => {
             <div>
                 <p>Vi har många års erfarenhet från vallning på olika nivåer. Spannet sträcker sig från glada motionärer till elitskidåkare i den absoluta eliten i både längdskidor och skidskytte. Vi har varit med och ”vallat hem” ett flertal Europa Cup, JVM och SM-medaljer genom åren.</p>
                 <p>Inför varje tävling registreras olika faktorer som indikerar val av produkter och slipar. Sen testas de olika produkterna för att hitta de produkter som är absolut bäst lämpad för aktuellt före. Vi har ett väl inarbetat system som vi arbetar mot och det har lett till ett flertal Europa Cup medaljer, JVM-medaljer och SM-medaljer genom åren.</p>
-                <p>Vi finns på plats under hela vasaloppsveckan och vallar inför alla tävlingar: Boka redan idag! Mer information finns under ”Information kring vasaloppet”.</p>
+                <p>Vi finns på plats under hela vasaloppsveckan och vallar inför alla tävlingar: Boka redan idag!</p>
             </div>
         </div>
     );
