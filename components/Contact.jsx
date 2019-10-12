b2f9f86fb388111a16afecc76a77ba6440ae996c
Magnus Linell <magnus.linell@gmail.com>
2019-10-12 14:10:42 +0200

    fskatt

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -14,6 +14,7 @@ export default () => {
                 <h2 className={styles.title}>Om oss</h2>
                 <p>Vi har många års erfarenhet från vallning på olika nivåer. Spannet sträcker sig från glada motionärer till elitskidåkare i den absoluta eliten i både längdskidor och skidskytte. Vi har varit med och ”vallat hem” ett flertal Europa Cup, JVM och SM-medaljer genom åren.</p>
                 <p>Inför varje tävling registreras olika faktorer som indikerar val av produkter och slipar. Sen testas de olika produkterna för att hitta de produkter som är absolut bäst lämpad för aktuellt före. Vi har ett väl inarbetat system som vi arbetar mot och det har lett till ett flertal Europa Cup medaljer, JVM-medaljer och SM-medaljer genom åren.</p>
+                <p>Vi innehar F-skattsedel.</p>
             </div>
         </>
     );
