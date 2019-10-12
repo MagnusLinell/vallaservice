9e75b0c6ab38bea8850cc71de45692b0d3cf0f29
Magnus Linell <magnus.linell@gmail.com>
2019-10-12 12:45:31 +0200

    changed name

--- pages/vallaservice.jsx
+++ pages/vallaservice.jsx
@@ -1,6 +1,6 @@
 import Header from '../components/Header';
 import Main from '../components/Main';
-import VallaService from '../components/VallaService';
+import VallaService from '../components/Vallaservice';
 import styles from './vallaservice.less';
 
 export default () => {
