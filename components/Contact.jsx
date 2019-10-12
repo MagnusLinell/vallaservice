416b1651cf0de2f9884a901e80bc213c335a4db0
Magnus Linell <magnus.linell@gmail.com>
2019-10-12 14:49:49 +0200

    fixed links

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,7 +6,7 @@ export default () => {
         <>
             <div className={styles.contact}>
                 <h1 className={styles.title}>Kontakt / Beställ</h1>
-                <div className={styles.row}>Ange produkt, antal skidor och lämningsdatum vid beställning. Upphämtning sker dagen efter.</div>
+                <div className={styles.description}>Ange produkt, antal skidor och lämningsdatum vid beställning. Upphämtning sker dagen efter.</div>
                 <div className={styles.row}><i className={classNames("fas", "fa-mobile", styles.icon)}></i>&nbsp;&nbsp;&nbsp;<a href="tel:070-23 55 223">070-23 55 223</a></div>
                 <div className={styles.row}><i className={classNames("fas", "fa-envelope", styles.icon)}></i> <a href="mailto:vallaservice@vallaservice.com">vallaservice@vallaservice.com</a></div>
                 <div className={styles.row}><i className={classNames("fas", "fa-home", styles.icon)}></i> Kråkstigen 78, 19823 Falköping</div>
