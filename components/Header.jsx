2058004e6fe25af7636e4238c9dc54ad4ddca7a0
Magnus Linell <magnus.linell@gmail.com>
2019-10-12 13:11:41 +0200

    rename

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,12 @@
-import Link from 'next/Link';
+import Link from 'next/link';
 import styles from './Header.less';
 
 export default () => {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <Link href="/"><a className={styles.link} >Vallamannen</a></Link>
-                <Link href="/vasaloppet"><a className={styles.link} >Vasaloppet</a></Link>
+                <Link href="/"><a className={styles.link}>Vallamannen</a></Link>
+                <Link href="/vasaloppet"><a className={styles.link}>Vasaloppet</a></Link>
                 <Link href="/vallaservice"><a className={styles.link}>Vallaservice</a></Link>
                 <Link href="/kontakt"><a className={styles.link}>Kontakt</a></Link>
             </nav>
