1835acadf8bc57dbe6f2ebd25387aa6e48afe5fb
Magnus Linell <magnus.linell@gmail.com>
2019-12-30 18:33:16 +0100

    optimize

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
+import 'normalize.css/normalize.css';
 import './_app.less';
+import Fingerprint2 from 'fingerprintjs2';
 
 class MyApp extends App {
+    componentDidMount() {
+        Fingerprint2.getV18((clientId) => {
+            ga('create', 'UA-155190132-1', {
+                'storage': 'none',
+                'clientId': clientId
+            });
+            ga('set', 'anonymizeIp', true);
+            ga('send', 'pageview');
+        });
+    }
+
     setGoogleTags() {
         return {
             __html:
                 `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag() {dataLayer.push(arguments); }
-                    gtag('js', new Date());
-                    gtag('config', 'ID');
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject'] = r;i[r]=i[r]||function(){
+                    (i[r].q = i[r].q || []).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
                 `
         };
     }
@@ -21,7 +34,7 @@ class MyApp extends App {
 
         return (
             <>
-                <Head>
+                <Head lang="sv_SE">
                     <title>Svensk Vallaservice</title>
                     <meta name="description" content="Svensk Vallaservice" />
                     <meta name="og:title" property="og:title" content="Svensk Vallaservice" />
@@ -31,7 +44,7 @@ class MyApp extends App {
                     <link href="/" rel="canonical" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=1" />
                     <meta property="og:site_name" content="Svensk Vallaservice" />
-                    <meta name="theme-color" content="#ffd32a" />
+                    <meta name="theme-color" content="#1e272e" />
                     <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
@@ -39,28 +52,21 @@ class MyApp extends App {
                     <link rel="manifest" href="/manifest.webmanifest" />
                     <meta charSet="utf-8" />
 
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=[ID]"></script>
+                    <link rel="preconnect" href="https://cdn.contentful.com" crossOrigin="anonymous" />
+                    <link rel="preconnect" href="https://images.ctfassets.net/" crossOrigin="anonymous" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="anonymous" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
 
-                    <script async dangerouslySetInnerHTML={this.setGoogleTags()} />
-
-                    <link 
-                        href="https://fonts.googleapis.com/css?family=Roboto&display=swap" 
-                        lazyload="true"
-                        rel="stylesheet" />
                     <link
+                        href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+                        lazyload="true"
+                        async
+                        defer
                         rel="stylesheet"
-                        type="text/css"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-                        lazyload="true"
-                    />
-                    <link rel="stylesheet"
-                        href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-                        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
-                        crossOrigin="anonymous"
-                        lazyload="true"
                     />
                 </Head>
                 <Component {...pageProps} />
+                <script async defer dangerouslySetInnerHTML={this.setGoogleTags()} />
             </>
         );
     }
