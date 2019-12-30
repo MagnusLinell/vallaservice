83b2006f27a4be707fedde8ef699935ee4217d41
Magnus Linell <magnus.linell@gmail.com>
2019-12-30 13:39:19 +0100

    changed design

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ export default () => {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <Link className={styles.link} href="/">Svensk Vallaservice</Link>
+                <Link className={styles.link} href="/" large>Svensk Vallaservice</Link>
                 <Link className={styles.link} href="/vasaloppet">Vasaloppet</Link>
                 <Link className={styles.link} href="/vallaservice">Vallaservice</Link>
                 <Link className={styles.link} href="/kontakt">Kontakt</Link>
--- components/Link.jsx
+++ components/Link.jsx
@@ -2,8 +2,8 @@ import classNames from 'classnames';
 import styles from './Link.less';
 import Link from 'next/link';
 
-export default ({ className, href, children }) => {
+export default ({ className, href, large, children }) => {
     return (
-        <Link href={href}><a className={classNames(styles.link, className)}>{children}</a></Link>
+        <Link href={href}><a className={classNames(styles.link, large && styles.large, className)}>{children}</a></Link>
     );
 }
